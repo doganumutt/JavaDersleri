0e02e2451c7d6e9a6750e8465b91a92f7552f380
doganumutt <umutdogan6217@gmail.com>
2015-02-04 12:11:05 +0200

    Merge origin/master
    Conflicts:
    	JavaDersleri/src/javadersleri/JavaDersleri.java

--- JavaDersleri/src/javadersleri/JavaDersleri.java
+++ JavaDersleri/src/javadersleri/JavaDersleri.java
@@ -1,29 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javadersleri;
 
-/**
- *
- * @author user
- */
 public class JavaDersleri {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
-        System.out.println("Deneme");//Umut//Mustafa
-
-        System.out.println("Deneme");//Umut dogan selam olsun
-
-        System.out.println("Deneme");
-
-
-        System.out.println("Deneme");//mustafa yorum satiri mustafa burada
     }
-
 }
