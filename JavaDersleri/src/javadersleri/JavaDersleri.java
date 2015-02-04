cbd274874a0c07c25a715fb1780f6da27d194207
doganumutt <umutdogan6217@gmail.com>
2015-02-04 12:08:25 +0200

    Merge origin/master
    Conflicts:
    	JavaDersleri/src/javadersleri/JavaDersleri.java

--- JavaDersleri/src/javadersleri/JavaDersleri.java
+++ JavaDersleri/src/javadersleri/JavaDersleri.java
@@ -15,7 +15,7 @@ public class JavaDersleri {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Deneme");//Umut
+        System.out.println("Deneme");//Umut//Mustafa
         System.out.println("Deneme");
 
 
